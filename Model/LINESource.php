06bb2973185b6aa71f640864a297c1c1c4364b66
Monk <ch.monk14@gmail.com>
2017-10-12 00:24:21 +0700

    used from response

--- Model/LINESource.php
+++ Model/LINESource.php
@@ -54,7 +54,7 @@ class LINEUser{
         $response = curl_exec($ch);
         curl_close($ch);
 
-//        echo $response;
+        echo $response['pictureUrl'];
 
         $JSONUser = var_dump(json_decode($response, true));
         echo '</br>decode'.$JSONUser['pictureUrl'];
