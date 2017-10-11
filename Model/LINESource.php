04751fbbc6cbde9d17e3a735da677b77b101d387
Monk <ch.monk14@gmail.com>
2017-10-12 00:20:20 +0700

    add curl transfer option

--- Model/LINESource.php
+++ Model/LINESource.php
@@ -48,6 +48,8 @@ class LINEUser{
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
 
         $response = curl_exec($ch);
         curl_close($ch);
